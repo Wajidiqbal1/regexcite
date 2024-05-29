44f675be7b3335108ba81436cbb6f88187396de6
wajid Iqbal <wajid.msbt581@iiu.edu.pk>
2024-05-29 12:00:27 +0100

    add documentation for strsplit1

--- R/strsplit1.R
+++ R/strsplit1.R
@@ -1,9 +1,7 @@
-x <- "apha,beta,charlie,delta"
-
-#' Title Split a string
+#' split a string
 #'
 #' @param x A character vector with one element
-#' @param split what to split on
+#' @param split A character string to split by
 #'
 #' @return A character vector
 #' @export
@@ -12,8 +10,6 @@ x <- "apha,beta,charlie,delta"
 #' x <- "alpha,beta,charlie,delta"
 #' strsplit1(x, ",")
 strsplit1 <- function(x, split){
-
-  strsplit(x, split = ",")[[1]]
-
+  strsplit(x, split = split)[[1]]
 }
-strsplit1(x, split = ",")
+
